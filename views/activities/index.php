ae41853b96a8071546488ae1b3817e3f7e0f2ddd
Max Lapko <maxlapko@gmail.com>
2014-12-16 18:31:57 +0300

    some fixes

--- views/activities/index.php
+++ views/activities/index.php
@@ -2,7 +2,10 @@
 $this->title = 'Public activities';
 
 ?>
+
+<h1><?= \yii\helpers\Html::encode($this->title) ?></h1>
 <?= \yii\widgets\ListView::widget([
     'dataProvider' => $provider,
-    'itemView' => '_view'
+    'itemView' => '_view',
+    'layout' => "{items}\n{pager}",
 ])?>
